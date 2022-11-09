e411e15acc7c08ab7d0f69a624f8406eb14c9867
KBogam <80017179+KBogam@users.noreply.github.com>
2022-11-08 19:50:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+noseX = 0;
+noseY = 0;
+
+
+
+function preload()
+{
+    mustache = loadImage('https://i.postimg.cc/SsqFb3GS/PNGPIX-COM-Moustache-PNG-Image-3.png');
+ 
+}
+
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function draw()
+{ 
+   image(video,0,0,300,300);
+
+   //fill(255,0,0);
+   //stroke(255,0,0);
+   //circle(noseX,noseY,15);
+
+   image(mustache,noseX,noseY,40,28);
+
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-20;
+        noseY = results[0].pose.nose.y+3;
+        console.log("Nose X = " + noseX);
+        console.log("Nose Y = " + noseY);
+        
+       
+    }
+}
+
+
+
+function take_snapshot()
+{
+   save('canvas.png'); 
+}
